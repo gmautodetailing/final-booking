6a54ac18240d770dc344cfdf8e29b7863f2adc4f
gmautodetailing <gmautodetailing.uk@gmail.com>
2025-07-19 16:15:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,8 @@ def index():
 def success():
     return render_template("success.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
